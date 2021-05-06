1f733f536e0ad39c3b303c15c1fbbf9373e8226d
Robert <robert.pastor0691@orange.fr>
2021-05-06 22:10:37 +0200

    re organise the code

--- helpers/azureSqlConnect.js
+++ helpers/azureSqlConnect.js
@@ -1,11 +1,12 @@
 'use strict'
 const Request = require('tedious').Request;
+const Connection = require('tedious').Connection;
 const { log } = require('./log')
+
 
 function executeStatement(connection) {
 
     return new Promise(function (resolve, reject) {
-
 
         let statement = "SELECT * FROM INFORMATION_SCHEMA.TABLES"
         log("execute Statement")
@@ -22,12 +23,72 @@ function executeStatement(connection) {
                 resolve()
             }
         })
-
         connection.execSql(request);
 
     })
 }
 
+function connect(configurationData) {
+
+    log(JSON.stringify(configurationData))
+    const config = {
+        server: configurationData.server, // or "localhost"
+
+        options: {
+            encrypt: true,
+            database: configurationData.database,
+            port: configurationData.port,
+            rowCollectionOnRequestCompletion: true
+
+        },
+        authentication: {
+            type: "default",
+            options: {
+                userName: configurationData.user,
+                password: configurationData.password,
+            }
+        }
+    };
+
+    config.options.debug = {
+        packet: true,
+        data: true,
+        payload: true,
+        token: true,
+        log: true
+    }
+
+    return new Promise(function (resolve, reject) {
+
+        var connection = new Connection(config);
+
+        // Setup event handler when the connection is established. 
+        connection.on('connect', function (err) {
+            if (err) {
+                log('Error: ', err)
+                reject(err)
+            } else {
+                // If no error, then good to go...
+                log("connection is OK")
+                resolve(connection)
+            }
+        });
+
+        connection.on('error', function (err) {
+            reject(err)
+        })
+
+        connection.on('debug', function (text) {
+            log(text)
+        });
+
+        // Initialize the connection.
+        connection.connect();
+    })
+
+}
+
 module.exports = {
-    executeStatement: executeStatement
+    executeStatement: executeStatement,
+    connect: connect
 }
--- index.js
+++ index.js
@@ -2,49 +2,14 @@
 "use strict";
 const { log } = require('./helpers/log')
 const { executeStatement } = require('./helpers/azureSqlConnect');
-
-const Connection = require('tedious').Connection;
+const { connect } = require('./helpers/azureSqlConnect')
 
 const configurationData = require('./configuration.json');
 
 log(JSON.stringify(configurationData))
 
-var config = {
-    server: configurationData.server, // or "localhost"
-
-    options: {
-        encrypt: true,
-        database: configurationData.database,
-        port: configurationData.port,
-        rowCollectionOnRequestCompletion: true
-
-    },
-    authentication: {
-        type: "default",
-        options: {
-            userName: configurationData.user,
-            password: configurationData.password,
-        }
-    }
-};
-
-config.options.debug = {
-    packet: true,
-    data: true,
-    payload: true,
-    token: true,
-    log: true
-}
-
-var connection = new Connection(config);
-
-// Setup event handler when the connection is established. 
-connection.on('connect', function (err) {
-    if (err) {
-        log('Error: ', err)
-    } else {
-        // If no error, then good to go...
-        log("connection is OK")
+connect(configurationData)
+    .then(connection => {
         executeStatement(connection)
             .then(_ => {
                 log("all is finished")
@@ -52,14 +17,9 @@ connection.on('connect', function (err) {
             })
             .catch(err => {
                 log(err)
+
             })
-
-    }
-});
-
-connection.on('debug', function (text) {
-    log(text)
-});
-
-// Initialize the connection.
-connection.connect();
+    })
+    .catch(err => {
+        log(err)
+    })
